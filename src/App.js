687cdc8e7957d63ce8aa83e29ef4f84b239fc571
Damian Savage <savage.damian@outlook.com>
2024-11-14 22:12:08 -0600

    added catalog page, and products component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar.jsx';
 import Footer from "./components/footer.jsx";
+import Products from './components/products.jsx';
 
 function App(){
   return(
     <div className='App'>
       <Navbar></Navbar>
       <h1 className='content'>Hello World React</h1>
+      <Products></Products>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/products.jsx
@@ -0,0 +1,11 @@
+import "./styles/products.css";
+
+function Products(){
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Products;
